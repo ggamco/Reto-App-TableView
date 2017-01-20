76294adf71d8fd2ae51c4f3e0d5b954d7f8fef26
cice <cice@aula8PovMacN08.local>
2017-01-20 19:11:39 +0100

    comentarios

--- IME_RetoAppTableView/IME_VistaTablaTVC.swift
+++ IME_RetoAppTableView/IME_VistaTablaTVC.swift
@@ -96,6 +96,7 @@ class IME_VistaTablaTVC: UITableViewController {
     
     //MARK: - CONTROL SEGUE CELDAS
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
         let ventanaDetalle = self.storyboard?.instantiateViewController(withIdentifier: "VistaDetalleContacto") as! IME_VentanaDetalleVC
         
         ventanaDetalle.dataContacto = contactos[indexPath.row]
@@ -151,13 +152,6 @@ class IME_VistaTablaTVC: UITableViewController {
         
     }
     
-    
-    
-    
-    
-    
-    
-
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
